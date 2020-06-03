7561e582f5689a61521467834d4f72ce90c9a5ec
Bilalahmedkamran <66181277+Bilalahmedkamran@users.noreply.github.com>
2020-06-03 12:26:54 +0500

    Update App.js
    Updated in app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Updated by Bilal A Kamran
         </p>
         <a
           className="App-link"
